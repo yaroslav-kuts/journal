0fb0cf4b173b5472cdf92f246ae821305d33a686
zzzonked <yaroslavkuts@gmail.com>
2019-03-11 20:52:48 +0200

    config user model relationships

--- app/Models/User.js
+++ app/Models/User.js
@@ -34,6 +34,10 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
+
+  posts () {
+    return this.hasMany('App/Models/Post')
+  }
 }
 
 module.exports = User
