d28b7a0e4741659d90c9d39c660bd3a9c47a5b16
zzzonked <yaroslavkuts@gmail.com>
2019-03-12 21:54:31 +0200

    apply created middleware

--- start/routes.js
+++ start/routes.js
@@ -1,35 +1,18 @@
-'use strict'
-
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
-
+const Route = use('Route');
 
 Route.get('/', () => ({ status: 'Ok', version: '1.0.0' }));
 
 Route.group(() => {
-    Route.post('authenticate', 'AuthController.authenticate');
+  Route.post('authenticate', 'AuthController.authenticate');
 
-    Route.get('users/:id', 'UserController.get');
-    Route.post('users', 'UserController.store');
-    Route.patch('users/:id', 'UserController.update')
-    Route.delete('users/:id', 'UserController.delete');
+  Route.get('users/:id', 'UserController.get').middleware(['findUser']);
+  Route.post('users', 'UserController.store');
+  Route.patch('users/:id', 'UserController.update').middleware(['findUser']);
+  Route.delete('users/:id', 'UserController.delete').middleware(['findUser']);
 
-    Route.get('posts/:id', 'PostController.get');
-    Route.get('posts', 'PostController.list');
-    Route.post('posts', 'PostController.store');
-    Route.patch('posts/:id', 'PostController.update')
-    Route.delete('posts/:id', 'PostController.delete');
-  }).prefix('api/v1');
+  Route.get('posts/:id', 'PostController.get').middleware(['findPost']);
+  Route.get('posts', 'PostController.list');
+  Route.post('posts', 'PostController.store');
+  Route.patch('posts/:id', 'PostController.update').middleware(['findPost']);
+  Route.delete('posts/:id', 'PostController.delete').middleware(['findPost']);
+}).prefix('api/v1');
