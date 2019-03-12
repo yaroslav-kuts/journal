eb46913a6d4c86513c21c2e8ea49460eee6e5b04
zzzonked <yaroslavkuts@gmail.com>
2019-03-12 22:03:04 +0200

    actualize post controller

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,40 +1,49 @@
-'use strict'
+const Post = use('App/Models/Post');
 
 class PostController {
-    async get({ request }) {
-        return {
-            message: 'ok',
-            data: {}
-        };
-    }
+  async get({ request }) {
+    const { $post } = request;
+    return {
+      message: 'ok',
+      data: $post,
+    };
+  }
 
-    async list() {
-        return {
-            message: 'ok',
-            data: {}
-        };
-    }
+  async list() {
+    const posts = await Post.all();
+    return {
+      message: 'ok',
+      data: posts,
+    };
+  }
 
-    async store({ request }) {
-        return {
-            message: 'Product was created',
-            data: {}
-        };
-    }
+  async store({ request }) {
+    const body = request.post();
+    const post = await Post.create(body);
+    return {
+      message: 'Post was created',
+      data: post,
+    };
+  }
 
-    async update({ request }) {
-        return {
-            message: 'Product was updated',
-            data: {}
-        };
-    }
+  async update({ request }) {
+    const { $post } = request;
+    const { text } = request.post();
+    $post.text = text;
+    return {
+      message: 'Post was updated',
+      data: $post,
+    };
+  }
 
-    async delete({ request }) {
-        return {
-            message: 'Product was deleted',
-            data: {}
-        };
-    }  
+  async delete({ request }) {
+    const { $post } = request;
+    await $post.delete();
+    return {
+      message: 'Post was deleted',
+      data: { id: $post.id },
+    };
+  }
 }
 
-module.exports = PostController
+module.exports = PostController;
