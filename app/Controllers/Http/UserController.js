6045e192be299ec1d2e39f2c2562923597e1eba0
zzzonked <yaroslavkuts@gmail.com>
2019-03-12 22:02:10 +0200

    actualize user controller

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,33 +1,45 @@
-'use strict'
+const User = use('App/Models/User');
 
 class UserController {
-    async get({ request }) {
-        return {
-            message: 'ok',
-            data: 'user'
-        };
-    }
+  async get({ request }) {
+    const { user } = request;
+    return {
+      message: 'ok',
+      data: user,
+    };
+  }
 
-    async store({ request }) {
-        return {
-          message: 'User was successfully created',
-          data: 'user'
-        };
-    }
+  async store({ request }) {
+    const body = request.post();
+    const user = await User.create(body);
+    return {
+      message: 'User was successfully created',
+      data: user,
+    };
+  }
 
-    async update({ request }) {
-        return {
-            message: 'User was updated',
-            data: {}
-        };
-    }
+  async update({ request }) {
+    const { user } = request;
+    const { firstname, lastname, email, verified } = request.post();
+    user.firstname = firstname;
+    user.lastname = lastname;
+    user.email = email;
+    user.verified = verified;
+    await user.save();
+    return {
+      message: 'User was updated',
+      data: user,
+    };
+  }
 
-    async delete({ request }) {
-        return {
-            message: 'User was deleted',
-            data: {}
-        };
-    }
+  async delete({ request }) {
+    const { user } = request;
+    await user.delete();
+    return {
+      message: 'User was deleted',
+      data: { id: user.id },
+    };
+  }
 }
 
-module.exports = UserController
+module.exports = UserController;
